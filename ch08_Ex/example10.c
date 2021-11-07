d6051dbee344b141fe715c3effe828e2617e7e98
abbiddo <79645200+abbiddo@users.noreply.github.com>
2021-11-07 23:16:11 +0900

    example10

---
+++ ch08_Ex/example10.c
@@ -0,0 +1,29 @@
+/**
+프로그램명 : example10.c
+설명 : 실행결과 처럼 출력되도록 해당 소스 코드의 빈칸을 채워라   
+작성일시 : 2021.11.07
+작성자 : 정소영
+**/
+
+#include <stdio.h>
+int main() {
+	int num1 = 1;
+	for (int i = 0;i < 10;i++){
+		switch (num1) {
+			case 1:
+				printf("1입니다.\n");
+				//______________
+				goto EXIT;
+			default:
+				break;
+		}
+	}
+	//___________
+	EXIT:
+		return 0;
+}
+
+/**
+실행 결과
+1입니다. 
+**/ 
