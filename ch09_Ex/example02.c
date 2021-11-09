a8abc93b8437b1374dcc7f7f66f013591d88ed8f
abbiddo <79645200+abbiddo@users.noreply.github.com>
2021-11-09 23:46:41 +0900

    문자열 인덱스

---
+++ ch09_Ex/example02.c
@@ -0,0 +1,21 @@
+/**
+프로그램명 : example02.c
+설명 : 실행결과에 맞게 해당 소스 코드의 빈칸을 채워라  
+작성일시 : 2021.11.09
+작성자 : 정소영
+**/
+
+#include <stdio.h>
+int main() {
+	char s1[30]="Beethoven 9th Symphony";
+	
+	//printf("%c\n", _________);
+	printf("%c\n",s1[10]);
+	
+	return 0; 
+}
+
+/**
+실행 결과
+9
+**/ 
