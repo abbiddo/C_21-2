87d15cf0b6a38b09219b62c2aade57f8a891cddb
abbiddo <79645200+abbiddo@users.noreply.github.com>
2021-11-09 23:29:39 +0900

    문자열

---
+++ ch09_Ex/example01.c
@@ -0,0 +1,19 @@
+/**
+프로그램명 : example01.c
+설명 : 해당 소스 코드의 빈칸을 채워라  
+작성일시 : 2021.11.07
+작성자 : 정소영
+**/
+
+#include <stdio.h>
+int main() {
+	//________________ = "Beethoven 9th Symphony";
+	char *s1="Beethoven 9th Symphony";
+	printf("%s\n",s1);
+	return 0; 
+}
+
+/**
+실행 결과
+Beethoven 9th Symphony
+**/ 
