e1847a92534c1e515ffe5c47bb86ab412bdec473
abbiddo <79645200+abbiddo@users.noreply.github.com>
2021-11-11 12:54:49 +0900

    문자열 붙이기

---
+++ ch09_Ex/example10.c
@@ -0,0 +1,25 @@
+/**
+프로그램명 : practice10.c
+설명 : 실행결과에 맞게 해당 소스 코드의 빈칸을 채워라   
+작성일시 : 2021.11.11
+작성자 : 정소영
+**/
+
+#include <stdio.h> 
+#include <string.h>
+int main(){
+	char s1[20]=" 9th Symphony";
+	char s2[40]="Beethoven";
+	
+	//______________________
+	strcat(s2,s1);
+	
+	printf("%s\n",s2);
+	
+	return 0;
+}
+
+/**
+실행 결과
+Beethoven 9th Symphony
+**/ 
