c6e4a07a181ddabc98330a6d82ea51a156ae77f5
abbiddo <79645200+abbiddo@users.noreply.github.com>
2021-11-11 12:07:52 +0900

    문자열 길이

---
+++ ch09_Ex/example06.c
@@ -0,0 +1,22 @@
+/**
+프로그램명 : practice06.c
+설명 : 실행결과에 맞게 해당 소스 코드의 빈칸을 채워라   
+작성일시 : 2021.11.11
+작성자 : 정소영
+**/
+
+#include <stdio.h> 
+#include <string.h>
+int main(){
+	char *s1 = "C Language";
+	
+	//printf("%d\n",___________);
+	printf("%d\n",strlen(s1));
+	
+	return 0;
+}
+
+/**
+실행 결과
+10
+**/ 
